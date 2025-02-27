cf668bb359433c12fb90949c8fbb7cfe1aa3b996
sergei_darin <swd86@mail.ru>
2025-02-27 13:50:49 +0400

    Убрал геттеры и сеттеры - добавил аннотацию @Data

--- src/main/java/ru/darin/non_working_days/dto/CountSearchDTO.java
+++ src/main/java/ru/darin/non_working_days/dto/CountSearchDTO.java
@@ -1,27 +1,13 @@
 package ru.darin.non_working_days.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 import ru.darin.non_working_days.util.validation.ValidSearchDTO;
 
+@Data
 @ValidSearchDTO
 @Schema(description = "Класс для передачи количества рабочих дней")
 public class CountSearchDTO {
-
     @Schema(description = "Количество рабочих дней", example = "3")
     private String count;
-
-    public String getCount() {
-        return count;
-    }
-
-    public void setCount(String count) {
-        this.count = count;
-    }
-
-    @Override
-    public String toString() {
-        return "CountSearchDTO{" +
-                "count='" + count + '\'' +
-                '}';
-    }
 }
--- src/main/java/ru/darin/non_working_days/dto/DateSearchDTO.java
+++ src/main/java/ru/darin/non_working_days/dto/DateSearchDTO.java
@@ -1,8 +1,10 @@
 package ru.darin.non_working_days.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 import ru.darin.non_working_days.util.validation.ValidSearchDTO;
 
+@Data
 @ValidSearchDTO
 @Schema(description = "Класс для поиска по диапазону дат.")
 public class DateSearchDTO {
@@ -11,28 +13,4 @@ public class DateSearchDTO {
 
     @Schema(description = "Верхняя граница диапазона (до)", example = "2025-02-23T18:56:06.898Z")
     private String dateTo;
-
-    public String getDateFrom() {
-        return dateFrom;
-    }
-
-    public void setDateFrom(String dateFrom) {
-        this.dateFrom = dateFrom;
-    }
-
-    public String getDateTo() {
-        return dateTo;
-    }
-
-    public void setDateTo(String dateTo) {
-        this.dateTo = dateTo;
-    }
-
-    @Override
-    public String toString() {
-        return "DateSearchDTO{" +
-                "dateFrom='" + dateFrom + '\'' +
-                ", dateTo='" + dateTo + '\'' +
-                '}';
-    }
 }
