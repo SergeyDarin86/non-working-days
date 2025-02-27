1fc43903ef6eea293f3a641df4b42cfd574d8327
sergei_darin <swd86@mail.ru>
2025-02-27 15:18:22 +0400

    добавил к проекту файл документации api-docs.yaml

--- src/main/java/ru/darin/non_working_days/dto/YearDTO.java
+++ src/main/java/ru/darin/non_working_days/dto/YearDTO.java
@@ -1,10 +1,13 @@
 package ru.darin.non_working_days.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import ru.darin.non_working_days.util.validation.ValidSearchDTO;
 
 @Data
 @ValidSearchDTO
+@Schema(description = "Класс для поиска данных по выходным дням за определенный год")
 public class YearDTO {
+    @Schema(description = "Год", example = "2025")
     private String year;
 }
